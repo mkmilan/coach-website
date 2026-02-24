a26e5b4d76b270c362dcf43b96c81be416401a5d
Milan Margetic <13180642+mkmilan@users.noreply.github.com>
2026-02-24 18:43:34 +0100

    instagram in footer

--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -1,17 +1,28 @@
 import Link from "next/link";
+import { FaInstagram } from "react-icons/fa6";
 import { localeHref } from "@/i18n/t";
 
+const INSTAGRAM_URL = "https://instagram.com/milan.endurance.lab";
+
 export default function SiteFooter({ locale, dict }) {
-  return (
-    <footer className="site-footer">
-      <div className="container site-footer__inner">
-        <p>{dict.common.footerLine}</p>
-        <div className="site-footer__links">
-          <Link href={localeHref(locale, "")}>{dict.common.home}</Link>
-          <Link href={localeHref(locale, "resources")}>{dict.common.resources}</Link>
-          <Link href={localeHref(locale, "contact")}>{dict.common.contact}</Link>
-        </div>
-      </div>
-    </footer>
-  );
+	return (
+		<footer className="site-footer">
+			<div className="container site-footer__inner">
+				<a
+					className="site-footer__instagram"
+					href={INSTAGRAM_URL}
+					target="_blank"
+					rel="noreferrer"
+					aria-label="Instagram"
+				>
+					<FaInstagram />
+				</a>
+				<div className="site-footer__links">
+					<Link href={localeHref(locale, "")}>{dict.common.home}</Link>
+					<Link href={localeHref(locale, "resources")}>{dict.common.resources}</Link>
+					<Link href={localeHref(locale, "contact")}>{dict.common.contact}</Link>
+				</div>
+			</div>
+		</footer>
+	);
 }
